ef351cef8dac7083155d27c1a861dd83e45889bc
theemmperor <96700328+theemmperor@users.noreply.github.com>
2023-03-24 10:37:21 -0700

    Hi

--- assets/scripts/functions.js
+++ assets/scripts/functions.js
@@ -12,3 +12,39 @@ showTime()
             document.getElementById('gas').src = "./assets/images/gas0.PNG";
         }
     }
+    
+    function setBrake() {
+        if (brakeLevel == 0) {
+            brakeLevel = 1;
+            document.getElementById('bttn_park').src = "./assets/images/bttn_park_0.PNG";
+            document.getElementById('brake').src = "./assets/images/p0.PNG";
+        } else {
+            brakeLevel = 0;
+            document.getElementById('bttn_park').src = "./assets/images/bttn_park_1.PNG";
+            document.getElementById('brake').src = "./assets/images/p1.PNG";
+        }
+    }
+
+    function setTrac() {
+        if (tracCtrl == 0) {
+            tracCtrl = 1;
+            document.getElementById('bttn_trac').src = "./assets/images/bttn_trac_0.PNG";
+            document.getElementById('trac').src = "./assets/images/trac1.PNG";
+        } else {
+            tracCtrl = 0;
+            document.getElementById('bttn_trac').src = "./assets/images/bttn_trac_1.PNG";
+            document.getElementById('trac').src = "./assets/images/trac0.PNG";
+        }
+    }
+
+    function chargeBattery() {
+        if (batCharge == 0) {
+            batCharge = 1;
+            document.getElementById('bttn_batt').src = "./assets/images/bttn_batt_1.PNG";
+            document.getElementById('batt').src = "./assets/images/batt1.PNG";
+        } else {
+            batCharge = 0;
+            document.getElementById('bttn_batt').src = "./assets/images/bttn_batt_0.PNG";
+            document.getElementById('batt').src = "./assets/images/batt0.PNG";
+        }
+    }
